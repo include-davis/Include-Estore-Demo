22d5486e8cab61b45cb4975dabf92703aeb3e578
Austin Shih <austinhshih@gmail.com>
2024-05-11 17:31:21 -0700

    Theme changes to reflect estore demo

--- app/(pages)/_components/Footer/Footer.tsx
+++ app/(pages)/_components/Footer/Footer.tsx
@@ -11,15 +11,12 @@ export default function Footer({ navLinks }: { navLinks: NavLink[] }) {
     <div className={styles.container}>
       <div className={styles.content_container}>
         <div className={styles.description}>
-          <h2>Platform Team Demo</h2>
-          <p>
-            This footer was designed so that you can practice writing efficient
-            HTML/CSS while getting familiar with our codebase.
-          </p>
+          <h2>include estore</h2>
+          <p>this estore demo was built for future clients to look at</p>
         </div>
         <div className={styles.navigation}>
           <div className={styles.learn_more}>
-            <h2>Learn more</h2>
+            <h2>look around</h2>
             <div className={styles.learn_more_links}>
               {navLinks.map((link) => {
                 return (
@@ -30,26 +27,10 @@ export default function Footer({ navLinks }: { navLinks: NavLink[] }) {
               })}
             </div>
           </div>
-          <div className={styles.projects}>
-            <h2>Projects</h2>
-            <div className={styles.project_link_columns}>
-              <div>
-                {/* Should be done with a loop */}
-                <Link href="/project/1">Project 1</Link>
-                <Link href="/project/2">Project 2</Link>
-                <Link href="/project/3">Project 3</Link>
-              </div>
-              <div>
-                <Link href="/project/4">Project 4</Link>
-                <Link href="/project/5">Project 5</Link>
-                <Link href="/project/6">Project 6</Link>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <p className={styles.credit}>
-        Designed & develped with 🤍 by #include at Davis @2023
+        Designed & develped with 🤍 by #include at Davis @2024
       </p>
     </div>
   );
