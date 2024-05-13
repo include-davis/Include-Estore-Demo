85d086628fb5d1a4c963d20a91f10343db1845ec
Austin Shih <austinhshih@gmail.com>
2024-05-12 21:21:30 -0700

    Simple Products Pages Done

--- app/(pages)/product/[id]/page.tsx
+++ app/(pages)/product/[id]/page.tsx
@@ -1,11 +1,23 @@
 'use client';
 import useProduct from '@hooks/useProduct';
-import styles from './page.module.scss';
+import ProductPage from '../_components/ProductPage/ProductPage';
+
+interface Product {
+  id: string;
+  src: string;
+  alt: string;
+  name: string;
+  price: number;
+}
 
 export default function Product({ params }: { params: { id: string } }) {
   const { product, loading } = useProduct(params.id);
   if (loading) {
     return 'loading...';
   }
-  return <div className={styles.container}>{JSON.stringify(product)}</div>;
+
+  if (product === null) {
+    return 'product does not exist';
+  }
+  return <ProductPage {...(product as Product)} />;
 }
