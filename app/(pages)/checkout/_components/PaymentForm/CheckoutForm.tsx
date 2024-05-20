d278166dfb6e3ea2bf2004ee477273f229df1f61
Mathias Gabel <matthiasj.gabel@gmail.com>
2024-05-20 10:34:36 -0700

    Return payment method

--- app/(pages)/checkout/_components/PaymentForm/CheckoutForm.tsx
+++ app/(pages)/checkout/_components/PaymentForm/CheckoutForm.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 
 import CardSection from './CardSection';
+import Stripe from 'stripe';
 
-export default function CheckoutForm({ secret }) {
+export default function CheckoutForm({ secret }: { secret: string }) {
   const stripe = useStripe();
   const elements = useElements();
+  const key = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
 
-  const handleSubmit = async (event) => {
+  const payment = new Stripe(`${key}`);
+
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
     // We don't want to let default form submission happen here,
     // which would refresh the page.
     event.preventDefault();
@@ -20,7 +24,7 @@ export default function CheckoutForm({ secret }) {
 
     const result = await stripe.confirmCardPayment(`${secret}`, {
       payment_method: {
-        card: elements.getElement(CardElement),
+        card: elements.getElement(CardElement)!,
         billing_details: {
           name: 'Jenny Rosen',
           email: 'matt@gmail.com',
@@ -39,6 +43,17 @@ export default function CheckoutForm({ secret }) {
         // execution. Set up a webhook or plugin to listen for the
         // payment_intent.succeeded event that handles any business critical
         // post-payment actions.
+        console.log('Payment success');
+        console.log(result.paymentIntent);
+        const intent = result.paymentIntent.payment_method;
+
+        const paymentMethod = await payment.paymentMethods.retrieve(
+          `${intent}`,
+          {
+            apiKey: `${key}`,
+          }
+        );
+        console.log(paymentMethod);
       }
     }
   };
